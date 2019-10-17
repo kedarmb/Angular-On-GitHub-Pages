d7e008dcfd0dc5669eddea597cae59a0fadfc2cf
GargAlish <alish.garg@eainfobiz.com>
2019-10-17 17:34:22 +0530

    organization setup screen

--- src/app/modal/organization-modal/organization-modal.component.ts
+++ src/app/modal/organization-modal/organization-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {Tender} from '../../model/tender.model';
 @Component({
   selector: 'app-organization-modal',
   templateUrl: './organization-modal.component.html',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationModalComponent implements OnInit {
 
-  constructor() { }
+  @Input('tender')
+  tender: Tender ;
+  placement = 'bottom';
+  constructor(public activeModal: NgbActiveModal) {}
 
   ngOnInit() {
+
   }
 
+
+  close() {
+    this.activeModal.close('closed');
+  }
 }
--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -1,4 +1,12 @@
+
 import { Component, OnInit } from '@angular/core';
+
+import {TenderModalComponent} from '../modal/tender-modal/tender-modal.component';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {TenderService} from '../service/tender.service';
+import {Tender} from '../model/tender.model';
+import {ActivatedRoute, Router} from '@angular/router';
+import { OrganizationModalComponent } from 'app/modal/organization-modal/organization-modal.component';
 
 @Component({
   selector: 'app-organization',
@@ -6,10 +14,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organization.component.scss']
 })
 export class OrganizationComponent implements OnInit {
-
-  constructor() { }
-
+  tender: any;
   ngOnInit() {
+    this.tenderService.getTenders().subscribe((result) => {
+      this.tender = result;
+    })
+  }
+
+  onDelete(index) {
+     this.tender.splice(index, 1);
+  }
+  constructor(private modalService: NgbModal, private tenderService: TenderService, private router: Router) {}
+
+  open(item?) {
+    const modalRef = this.modalService.open(OrganizationModalComponent, {centered: true});
+    modalRef.componentInstance.tender = item || new Tender();
+
+  }
+  viewTender() {
+    this.router.navigateByUrl('view-tender');
   }
 
 }
