9b06746a28b09e72eb2db0279a4159ee3ca1dc80
vikas bansal <vikas.bansal@thinkfect.com>
2019-10-19 18:47:43 +0530

    bug fixed

--- src/app/directive/auto-adjust.directive.ts
+++ src/app/directive/auto-adjust.directive.ts
@@ -18,9 +18,9 @@ export class AutoAdjustDirective implements AfterViewInit {
   }
    autosize() {
     setTimeout(() => {
-      let height = parseInt(this.elementRef.nativeElement.scrollHeight);
+      const height = parseInt(this.elementRef.nativeElement.scrollHeight,10);
       if (height > 0) {
-      this.elementRef.nativeElement.style.height =  this.elementRef.nativeElement.scrollHeight + 'px';
+      this.elementRef.nativeElement.style.cssText = 'height : ' + this.elementRef.nativeElement.scrollHeight + 'px !important';
       }
     }, 0);
   }
