3f50ca9b8c5d8ae56e09441ed888f1d3b03ab9d9
vikas bansal <vikas.bansal@thinkfect.com>
2019-10-21 20:05:56 +0530

    remove wegl

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {TenderService} from '../service/tender.service';
 import {Tender} from '../model/tender.model';
 
@@ -7,7 +7,7 @@ import {Tender} from '../model/tender.model';
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.scss']
 })
-export class QuoteComponent implements OnInit {
+export class QuoteComponent implements OnInit, AfterViewInit {
 
   tender: Tender;
   contractor = true;
@@ -26,6 +26,39 @@ export class QuoteComponent implements OnInit {
 
   lineItemWise() {
     this.contractor = false;
+  }
+  ngAfterViewInit(): void {
+    this.initClickAndDrag();
+  }
+
+  initClickAndDrag() {
+    const slider = document.querySelector('.items');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = (<any>e).pageX - (<any>slider).offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if (!isDown) { return; }
+      e.preventDefault();
+      const x = (<any>e).pageX - (<any>slider).offsetLeft;
+      const walk = (x - startX) * 3; // scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+      console.log(walk);
+    });
   }
 
 }
