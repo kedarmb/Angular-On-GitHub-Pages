004ec8218a5c405257af9be6163811f73691c67c
Ankur Shukla <ashukla@thinkperfect.io>
2019-11-11 16:35:28 +0530

    adds validation on signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { PasswordValidator } from './password.validator';
+import { SignupFormGroup } from './signup.validator';
+// import { PasswordValidator } from './password.validator';
+import {SignupFormControl} from './signup.validator';
+// import {signupFormGroup} from './signup.validator';
+import {TooltipPosition} from '@angular/material/tooltip';
 import { from } from 'rxjs';
 
 @Component({
@@ -9,15 +13,19 @@ import { from } from 'rxjs';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  registrationForm:SignupFormGroup=new SignupFormGroup();
+  formSubmitted: boolean = false;
+  positionOptions: TooltipPosition[] = ['below'];
+  position = new FormControl(this.positionOptions[0]);
 
   constructor(private fb : FormBuilder) { }
-  registrationForm=this.fb.group({
-    orgName:['', [Validators.required]],
-    name:['', [Validators.required]],
-    email:['', [Validators.required]],
-    password:['',[Validators.required]],
-    confirmPassword:['',[Validators.required]]
-  }, {validator:PasswordValidator});
+  // registrationForm=this.fb.group({
+  //   // orgName:['', [Validators.required]],
+  //   name:['', [Validators.required]],
+  //   email:['', [Validators.required]],
+  //   password:['',[Validators.required]],
+  //   confirmPassword:['',[Validators.required]]
+  // }, {validator:PasswordValidator});
   ngOnInit() {
   }
 
---
+++ src/app/signup/signup.validator.ts
@@ -0,0 +1,61 @@
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+// import { PasswordValidator } from './password.validator';
+// import { AbstractControl } from "@angular/forms";
+export class SignupFormControl extends FormControl {
+    modelProperty: string;
+    label: string;
+
+    constructor(label: string, modelProperty: string, value: any, validator: any) {
+        super(value, validator);
+        this.modelProperty = modelProperty;
+        this.label = label;
+    }
+
+    getValidationMessages() {
+        let messages: string[] = [];
+        if (this.errors) {
+            for (let errorName in this.errors) {
+                switch (errorName) {
+                    case 'required':
+                        messages.push(`You must enter a ${this.label}`);
+                        break;
+                    case 'minlength':
+                        messages.push(`A ${this.label} must be at least ${this.errors['minlength'].requiredLength} characters`);
+                        break;
+                    case 'maxlength':
+                        messages.push(`A ${this.label} must be no more than ${this.errors['maxlength'].requiredLength} characters`);
+                        break;
+                    case 'pattern':
+                        messages.push(`The ${this.label} is invalid`);
+                        break;
+                }
+            }
+        }
+        return messages;
+    }
+}
+
+export class SignupFormGroup extends FormGroup {
+    constructor() {
+        super({
+            orgName: new SignupFormControl('orgName', 'orgName', '', Validators.compose([Validators.required, Validators.minLength(5),Validators.maxLength(30)])),
+            name: new SignupFormControl('name','name','', Validators.compose([Validators.required])),
+            email: new SignupFormControl('email', 'email', '', Validators.compose([Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])),
+            password:new SignupFormControl('password','password','', Validators.compose([Validators.required, Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}'),Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')])),
+            confirmPassword:new SignupFormControl('confirmPassword','confirmPassword','', Validators.compose([Validators.required]))
+        })
+    }
+
+    get parentalControls(): SignupFormControl[] {
+
+        return Object.keys(this.controls).map((k) => this.controls[k] as SignupFormControl);
+    }
+
+    getFormValidationMessages(): string[] {
+        let messages: string[] = [];
+        this.parentalControls.forEach(c => c.getValidationMessages().forEach(m => messages.push(m)));
+        return messages;
+    }
+
+
+}
