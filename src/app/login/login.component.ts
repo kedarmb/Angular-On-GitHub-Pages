9c1e3209bb873b1286ab7aa47e1e6c4b2ada2d8c
trathod7 <trathod@thinkperfect.io>
2019-11-22 13:56:58 +0530

    new form code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,14 @@
+import { regex } from './../constant/index';
+import { HelperService } from './../service/helper.service';
+import { UserServiceService } from './../service/user-service.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import {LoginFormControl} from './login.validator';
-import {LoginFormGroup} from './login.validator';
-import {Router} from '@angular/router';
+// import { LoginFormControl } from './login.validator';
+// import { LoginFormGroup } from './login.validator';
+import { Router } from '@angular/router';
 import Login from 'app/model/login.model';
-import {LoginService} from '../service/login.service';
+import { LoginService } from '../service/login.service';
+import {errorMsg, regex} from '../constant/index'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,36 +16,32 @@ import {LoginService} from '../service/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  form: LoginFormGroup = new LoginFormGroup();
+  form: FormGroup;
   formSubmitted: boolean = false;
-  loginObj:Login= new Login();
-  constructor(private router:Router , private loginService: LoginService) { }
-
-  ngOnInit() {
+  loginObj: Login = new Login();
+  constructor(private router: Router,
+    private formBuider: FormBuilder,
+    private loginService: LoginService, 
+    private helperService: HelperService){ 
+    }
+    
+    ngOnInit() {
+      // console.log(this.emailRegex)
+    this.form = this.formBuider.group({
+      email: ['', [Validators.required, this.helperService.customPatternValid({ pattern: regex.emailReg , msg:errorMsg.email})]],
+      password: ['', Validators.required]
+    })
   }
 
-   submit(form){
-     this.formSubmitted = true;
-     if (form.valid) {
-      console.log(form.controls.email.value);
-      console.log(form.controls.password.value);
-      this.loginObj.email = form.controls.email.value;
-      this.loginObj.password = form.controls.password.value;
-      //
-
-      this.loginService.login(this.loginObj).subscribe(data=>
-        {
-          console.log(this.loginObj)
-          console.log(data);
-          
-          this.formSubmitted = false;
-           this.router.navigateByUrl('/dashboard');
-        })
-      
-      
-     }else{
-       console.log('***************************Form is invalid');
-     }
-
+  submit(form) {
+    console.log(this.form);
+    // this.loginService.login(this.form).subscribe((response: any) => {
+    //   if (response.status === 200) {
+    //     console.log(response);
+    //     this.router.navigateByUrl('/dashboard');
+    //   }
+    // }, error => {
+    //   console.log(error);
+    // });
   }
 }
