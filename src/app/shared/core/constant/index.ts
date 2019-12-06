dd9fd555d7de03b4167b8ecc5480029c24eebcd5
AnkurShukla215 <56726470+AnkurShukla215@users.noreply.github.com>
2019-12-06 15:31:02 +0530

    Ankur development (#42)
    * adds validations to organization form
    
    * add user form
    
    * Forms for Labour, Equipments, & users
    
    * added validation to the user form .
    called APIs
    
    * styling in crew component
    
    * validations for add organization & add-user forms

--- src/app/shared/core/constant/index.ts
+++ src/app/shared/core/constant/index.ts
@@ -6,8 +6,8 @@ alphaFive: '',
 alphaThirty: '',
 emailReg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 organizationName: '',
-alphabetOnly: '^[^-\s][a-zA-Z0-9_\s-]+$', // should not contain numbers & special Charecters,
-phoneNumber: /^[6-9]\d{9}$/,
+  alphabetOnly: /^[A-Za-z]+$/, // should not contain numbers & special Charecters,
+  phoneNumber: /([0-9\s\-]{7,})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/,
 
   passwordPattern: '(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{8,}'
 };
@@ -15,8 +15,8 @@ export const errorMsg = {
       email: 'Email address is not valid',
       nameErr: 'Name must be at least 5 chars, must not exceed 30 chars & must not start or end with special chars',
       orgName: 'Organization Name',
-      nameMessage:  'This field should not start with space and should not contain numbers & special Charecters',
+      nameMessage:  'This field should not start with any special charecters and should not contain special charecters',
       passwordMessage: 'Password should be at least 8 digits & should contain one number and one special charecter',
       requiredField: 'This field is required',
-      phoneMsg: 'Phone number should have max 10 digits & should not contain any special charecters'
+      phoneMsg: 'Please enter valid phone number'
       };
