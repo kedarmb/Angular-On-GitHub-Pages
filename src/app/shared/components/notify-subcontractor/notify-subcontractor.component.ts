6ab918ee24798ad11bb02814e400438cb1703c5f
AnkurShukla215 <56726470+AnkurShukla215@users.noreply.github.com>
2020-01-28 12:47:25 +0530

    Ankur development (#103)
    * adds validations to organization form
    
    * add user form
    
    * Forms for Labour, Equipments, & users
    
    * added validation to the user form .
    called APIs
    
    * styling in crew component
    
    * validations for add organization & add-user forms
    
    * conversion to "Mat-Dialog"
    
    * conversion of ngb modal to mat-dialog
    
    * create crew changes in component  structure
    
    * "Updation of API Url" for  Organization
    
    * integration with API for Organization
    
    * "Validation and API response handeling"
    
    * CRUD done for USER
    
    * minor changes in user
    
    * added ellipses and another minor changes
    
    * changes in tender component merge
    
    * APIs integration for Labour/Equipment
    
    * minor changes for equipments
    
    * post merge changes
    
    * update angular cli and core
    
    * saved mat select search
    
    * Node module installed
    
    * Changes for crew Get/POST/Delete
    
    * PUT API working
    
    * Trench Template Done
    
    * Crew(GET/POST/PUT/DELETE) Working
    
    * Trench Calculator UI
    
    * GET/POST/DELETE api working
    
    * POST Api for trench
    
    * "changes for Trench calculation
    
    * notification to the Sub(without API)

--- src/app/shared/components/notify-subcontractor/notify-subcontractor.component.ts
+++ src/app/shared/components/notify-subcontractor/notify-subcontractor.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HttpService } from 'app/shared/core/service/http.service';
 import Organization from 'app/shared/core/model/organization.model';
+import { FormGroup, FormBuilder, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-notify-subcontractor',
@@ -12,42 +13,64 @@ import Organization from 'app/shared/core/model/organization.model';
   // providers: [MatDialogRef]
 })
 export class NotifySubcontractorComponent implements OnInit {
+  inviteSub: FormGroup;
   [x: string]: any;
+  clicked: true;
 
   organizations: any = [];
   constructor(/* private activeModal: NgbActiveModal, */ private httpService: HttpService,
     public subContDialogueRef: MatDialogRef<NotifySubcontractorComponent>,
+    private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any) {
-    }
-
-    ngOnInit() {
-    this.getOrganization();
-      /*
-        this.httpService.getAll().subscribe((organizations) => {
-        this.organizations = organizations;
-      })*/
-      // this.organizations = [{ name: 'Subcontractor A' }, { name: 'Subcontractor B' }, { name: 'Subcontractor C' }]
+    this.inviteSub = this.fb.group({
+      orgData: this.fb.array([], [Validators.required])
+    })
   }
+
+  ngOnInit() {
+    this.getOrganization();
+  }
+
+  onCheckboxChange(e) {
+    const orgData: FormArray = this.inviteSub.get('orgData') as FormArray;
+    if (e.target.checked) {
+      orgData.push(new FormControl(e.target.value));
+    } else {
+      console.log('return:', e.target.value);
+      orgData.value.map((item: FormControl, i) => {
+        if (item === e.target.value) {
+          orgData.removeAt(i);
+          return;
+        }
+      });
+    }
+  }
+
   getOrganization() {
     this.httpService.getAllOrganization()
       .subscribe((response: any) => {
         if (response.status === 200) {
           const organizations = response.body;
           console.log(organizations)
-            this.organizations =  organizations.filter((obj) => {
-                if (obj.orgType[0] === 'Prime') {
-                  return obj
-                }
-              })
-
-          console.log('org:', this.organizations);
+          this.organizations = organizations.filter((obj) => {
+            console.log('obj', obj);
+            if (obj.orgType[0] === 'Sub') {
+              return obj
+            }
+          })
         }
       }, error => {
         this.toastr.error(error.error.message)
       })
   }
+
+  submitForm() {
+    console.log(this.inviteSub.value)
+  }
+
   close() {
     this.subContDialogueRef.close(this.data);
     console.log('dialog closed ', this.data)
   }
+
 }
