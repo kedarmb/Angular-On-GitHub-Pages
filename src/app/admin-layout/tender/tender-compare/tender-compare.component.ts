aead448537fd058f873cfea9e82e6495b6d64d4c
AnkurShukla215 <56726470+AnkurShukla215@users.noreply.github.com>
2020-02-01 15:10:33 +0530

    Ankur development (#108)
    * adds validations to organization form
    
    * add user form
    
    * Forms for Labour, Equipments, & users
    
    * added validation to the user form .
    called APIs
    
    * styling in crew component
    
    * validations for add organization & add-user forms
    
    * conversion to "Mat-Dialog"
    
    * conversion of ngb modal to mat-dialog
    
    * create crew changes in component  structure
    
    * "Updation of API Url" for  Organization
    
    * integration with API for Organization
    
    * "Validation and API response handeling"
    
    * CRUD done for USER
    
    * minor changes in user
    
    * added ellipses and another minor changes
    
    * changes in tender component merge
    
    * APIs integration for Labour/Equipment
    
    * minor changes for equipments
    
    * post merge changes
    
    * update angular cli and core
    
    * saved mat select search
    
    * Node module installed
    
    * Changes for crew Get/POST/Delete
    
    * PUT API working
    
    * Trench Template Done
    
    * Crew(GET/POST/PUT/DELETE) Working
    
    * Trench Calculator UI
    
    * GET/POST/DELETE api working
    
    * POST Api for trench
    
    * "changes for Trench calculation
    
    * notification to the Sub(without API)
    
    * Tender-bid Table created
    
    * "Bid Table created"
    
    * added save button
    
    * get API integrated for compare

--- src/app/admin-layout/tender/tender-compare/tender-compare.component.ts
+++ src/app/admin-layout/tender/tender-compare/tender-compare.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { HttpService } from 'app/shared/core/service/http.service';
+import { id } from 'date-fns/locale';
 
 @Component({
   selector: 'app-tender-compare',
@@ -8,358 +10,32 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TenderCompareComponent implements OnInit {
-  tender = {
-    'name': 'tender Pipe line laying',
-    'clientRef': '',
-    'description': 'String',
-    'openDate': 'String',
-    'closeDate': 'String',
-    'quoteStartDate': 'String',
-    'quoteEndDate': 'String',
-    'sections': [
-      {
-        'name': 'section 1',
-        'sectionTotal': [{
-          'totalPrice': 'Number',
-          'quoteSub': 'sf444'
-        }],
-
-        'lineItems': [
-          {
-            'specNo': '561000Sno',
-            'itemNo': '981000Ino',
-            'name': 'line 3',
-            'description': 'String',
-            'unit': 'String',
-            'quantity': '150',
-            'notifiedSubs': [
-              '1',
-              '2'
-            ],
-            'selectedSub': [
-              '1'
-            ],
-            'subLineItems': [
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf444'
-                  }
-                ],
-
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              },
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf445'
-                  }
-                ],
-
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              }
-            ],
-            'lineItemTotal': [
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf444'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf445'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf446'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf447'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf448'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf449'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf450'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf451'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              }, {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              }, {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              }, {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              }, {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              }, {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              }, {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf452'
-              }
-
-
-            ],
-          },
-          {
-            'specNo': '561001Sno',
-            'itemNo': '981001Ino',
-            'name': 'line 4',
-            'description': 'String',
-            'unit': 'String',
-            'quantity': '300',
-            'notifiedSubs': [
-              '1',
-              '2'
-            ],
-            'selectedSub': [
-              '1'
-            ],
-            'subLineItems': [
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf444'
-                  }
-                ],
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              },
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf445'
-                  }
-                ],
-
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              }
-            ],
-            'lineItemTotal': [
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf444'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf445'
-              }
-            ],
-          }
-        ],
-        'status': 'Created'
-      },
-      {
-        'name': 'section 2',
-        'sectionTotal': [{
-          'totalPrice': 'Number',
-          'quoteSub': 'sf444'
-        }],
-
-        'lineItems': [
-          {
-            'specNo': '561002Sno',
-            'itemNo': '981002Ino',
-            'name': 'line 1',
-            'description': 'String',
-            'unit': 'String',
-            'quantity': '800',
-            'notifiedSubs': [
-              '1',
-              '2'
-            ],
-            'selectedSub': [
-              '1'
-            ],
-            'subLineItems': [
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf444'
-                  }
-                ],
-
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              },
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf445'
-                  }
-                ],
-
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              }
-            ],
-            'lineItemTotal': [
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf444'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf445'
-              }
-            ],
-          },
-          {
-            'specNo': '561003Sno',
-            'itemNo': '981003Ino',
-            'name': 'line 2',
-            'description': 'String',
-            'unit': 'String',
-            'quantity': '500',
-            'notifiedSubs': [
-              '1',
-              '2'
-            ],
-            'selectedSub': [
-              '1'
-            ],
-            'subLineItems': [
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf444'
-                  }
-                ],
-
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              },
-              {
-                'name': 'String',
-                'quotes': [
-                  {
-                    'unit': 'String',
-                    'quantity': '5',
-                    'unitPrice': '50',
-                    'totalPrice': '250',
-                    'quoteSub': 'sf445'
-                  }
-                ],
-
-                'trenchRef': 'sda5455',
-                'crewRef': 'sdf545'
-              }
-            ],
-            'lineItemTotal': [
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf444'
-              },
-              {
-                'unitPrice': 'Number',
-                'totalPrice': 'N  umber',
-                'quoteSub': 'sf445'
-              }
-            ],
-          }
-        ],
-        'status': 'Created'
-      }
-    ],
-    'createdDate': '4564646',
-    'createdBy': '6546464664654',
-    'updatedDate': '65464646656231',
-    'updatedBy': '54646461asdsaas',
-    'organizationRef': '45sdfs'
-  }
   lineTotal: any;
   selection;
+  tenderCompare;
+    displayedColumns: string[] = ['select', 'SubContractor', 'unitPrice', 'totalPrice'];
+  constructor( private httpService: HttpService) {
 
-  constructor() { }
+    this.getAllTenders();
+  }
 
   ngOnInit() {
-    console.log(this.tender);
+    console.log(this.tenderCompare);
     // this.fetchLineTotal();
   }
   fetchLineTotal(sec, lt, slt) {
-    this.lineTotal = this.tender.sections[sec].lineItems[lt].lineItemTotal.filter(val => val.quoteSub == slt)
+    this.lineTotal = this.tenderCompare.sections[sec].lineItems[lt].lineItemTotal.filter(val => val.quoteSub === slt)
     console.log(this.lineTotal);
   }
-  displayedColumns: string[] = ['select', 'SubContractor', 'unitPrice', 'totalPrice'];
- 
+
+  getAllTenders() {
+    return this.httpService.getTenders().subscribe((response) => {
+      if (response.status === 200) {
+        this.tenderCompare = response.body;
+        console.log(this.tenderCompare)
+      }
+    }, (error) => {
+      console.log('err in fetching tender headers ', error);
+    })
+  }
 }
